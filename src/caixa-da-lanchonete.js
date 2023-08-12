8d48b78a828a89af5b17efeadc210465a04c198e
layconjohn <layconjohn@gmail.com>
2023-08-12 13:59:17 -0300

    feat: valida metodo de pagamento

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,11 +1,19 @@
 class CaixaDaLanchonete {
 
     calcularValorDaCompra(metodoDePagamento, itens) {
-
+        if (!this.validaMetodoPagamento(metodoDePagamento)) {
+            return "Forma de pagamento inválida!"
+        }
 
         return "";
     }
 
+    validaMetodoPagamento(metodoDePagamento) {
+        if (metodoDePagamento === "dinheiro" || metodoDePagamento === "debito" || metodoDePagamento === "credito") {
+            return true;
+        }
+        return false;
+    }
     
 }
 
